5a6d0293c069a9e60853e34b595ee03e9fe6d8ba
ivanmhale <ivanmhale@gmail.com>
2020-02-04 10:21:47 -0500

    imported material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { ResultsComponent } from './results/results.component';
-import { ModalComponent } from './modal/modal.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from "./app.component";
+import { NavComponent } from "./nav/nav.component";
+import { ResultsComponent } from "./results/results.component";
+import { ModalComponent } from "./modal/modal.component";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./material.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent,
-    ResultsComponent,
-    ModalComponent
-  ],
-  imports: [
-    BrowserModule,
-    NoopAnimationsModule
-  ],
+  declarations: [AppComponent, NavComponent, ResultsComponent, ModalComponent],
+  imports: [BrowserModule, NoopAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
